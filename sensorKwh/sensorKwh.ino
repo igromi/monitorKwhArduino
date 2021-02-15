5b421f6afdec5bfafced2f6b1458c37cc3e86da1
agui1era <oscar@aguilera.xyz>
2021-02-15 13:12:23 -0300

    15022021

--- sensorKwh/sensorKwh.ino
+++ sensorKwh/sensorKwh.ino
@@ -1,5 +1,5 @@
 #include <PZEM004Tv30.h>
-
+#define id "123456789"
 
 SoftwareSerial Serial1(10, 11); // RX, TX
 PZEM004Tv30 pzem(8,9);
@@ -12,13 +12,18 @@ void setup() {
 void loop() {
      float voltage = pzem.voltage();
      if(voltage != NAN){
-         Serial.print("{\"V\":");
+         Serial.print("{\"V");
+         Serial.print(id);
+         Serial.print("\":");
          Serial.print(voltage);
          Serial.print(","); 
          
-         Serial1.print("{\"V\":");
+         Serial1.print("{\"V");
+         Serial1.print(id);
+         Serial1.print("\":");
          Serial1.print(voltage);
          Serial1.print(","); 
+          
      } else {
          Serial.println("Error reading voltage");
      }
@@ -26,11 +31,15 @@ void loop() {
      float current = pzem.current();
      if(current != NAN){
          
-         Serial.print("\"A\":");
+         Serial.print("\"A");
+         Serial.print(id);
+         Serial.print("\":");
          Serial.print(current);
          Serial.print(","); 
       
-         Serial1.print("\"A\":");
+         Serial1.print("\"A");
+         Serial1.print(id);
+         Serial1.print("\":");
          Serial1.print(current);
          Serial1.print(","); 
        
@@ -42,13 +51,17 @@ void loop() {
      float power = pzem.power();
      if(current != NAN){
 
-         Serial.print("\"W\":");
+         Serial.print("\"W");
+         Serial.print(id);
+         Serial.print("\":");
          Serial.print(power);
          Serial.print(",");
 
-         Serial1.print("\"W\":");
+         Serial1.print("\"W");
+         Serial1.print(id);
+         Serial1.print("\":");
          Serial1.print(power);
-         Serial1.print(","); 
+         Serial1.print(",");
          
      } else {
          Serial.println("Error reading power");
@@ -57,13 +70,17 @@ void loop() {
      float energy = pzem.energy();
      if(current != NAN){
 
-         Serial.print("\"KWH\":");
+         Serial.print("\"KWH");
+         Serial.print(id);
+         Serial.print("\":");
          Serial.print(energy);
          Serial.print(","); 
 
-         Serial1.print("\"KWH\":");
+         Serial1.print("\"KWH");
+         Serial1.print(id);
+         Serial1.print("\":");
          Serial1.print(energy);
-         Serial1.print(","); 
+         Serial1.print(",");
          
      } else {
          Serial.println("Error reading energy");
@@ -73,11 +90,15 @@ void loop() {
      float pf = pzem.pf();
      if(current != NAN){
 
-         Serial.print("\"FactorP\":");
+         Serial.print("\"FactorP");
+         Serial.print(id);
+         Serial.print("\":");
          Serial.print(pf);
          Serial.print("}");
 
-         Serial1.print("\"FactorP\":");
+         Serial1.print("\"FactorP");
+         Serial1.print(id);
+         Serial1.print("\":");
          Serial1.print(pf);
          Serial1.print("}");
          delay(5000);
